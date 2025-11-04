ef436a64211369f08b5117ba4abe17b7157b68d7
Khafeefa <kelmalaki11@gmail.com>
2025-11-03 21:00:12 -0700

    Update site title and favicon to pink ribbon.

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Breast Cancer Hope Initiative ASU</title>
+        <link rel="icon" href="/pink-ribbon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
